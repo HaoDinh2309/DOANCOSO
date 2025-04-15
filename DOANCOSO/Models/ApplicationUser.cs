9055e121f4ef1c5d0dc803f3f216f9c72b55d325
HaoDinh2309 <dvhao.c4pbchau2122@gmail.com>
2025-04-15 22:16:12 +0700

    Add project files.

---
+++ DOANCOSO/Models/ApplicationUser.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace DOANCOSO.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        [Required]
+        public string FullName { get; set; }
+
+        public string? Age { get; set; }
+
+        // Ngày tháng năm sinh
+        [DataType(DataType.Date)]
+        public DateTime? DateOfBirth { get; set; }
+
+        // Avatar người dùng (URL hoặc đường dẫn đến file)
+        public string Avatar { get; set; }
+    }
+}
